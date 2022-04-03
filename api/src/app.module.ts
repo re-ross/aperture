c954a77f8ca59109ab584b42551463b67b472e07
Ryan Ross <ryan@reross.com>
2022-04-03 18:33:48 -0500

    generated auth module, service, and controller. installed passport and bcrypt

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersService } from './modules/users/users.service';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
