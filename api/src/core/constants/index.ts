4f25827cd4d7ff48326d3cb4a3498eb3da734bec
Ryan Ross <ryan@reross.com>
2022-04-07 13:50:39 -0500

    refactoring

--- api/src/core/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export const SEQUELIZE = 'SEQUELIZE';
-export const DEVELOPMENT = 'development';
-export const TEST = 'test';
-export const PRODUCTION = 'production';
-export const USER_REPOSITORY = 'USER_REPOSITORY';
